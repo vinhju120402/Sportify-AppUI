8917a9c0783295de2ab66dd095399c465f87c453
Vinh Tran <101914702+vinhju120402@users.noreply.github.com>
2024-06-20 11:03:24 +0200

    Add files via upload

---
+++ lib/app/model/album_images.dart
@@ -0,0 +1,26 @@
+List albumImages = [
+  'https://i.scdn.co/image/ab67706f000000037988283d13d5654287988494',
+  'https://thisis-images.scdn.co/37i9dQZF1DZ06evO3Jefw4-default.jpg',
+  'https://i.scdn.co/image/ab67706f00000003387c0ddfa4c7553ce24edba5',
+  'https://i.scdn.co/image/ab67706f00000003e94b9c30b642a8858703fc31',
+  'https://thisis-images.scdn.co/37i9dQZF1DZ06evO2YqUuI-default.jpg',
+  'https://i.scdn.co/image/ab67706f000000037cda1a881997b0bb1ca0eb92',
+];
+
+List songImages = [
+  'https://i.scdn.co/image/ab67616d0000b2731624590458126fc8b8c64c2f',
+  'https://i.scdn.co/image/ab67706c0000bebb5e0d1386755fc7757591d2b5',
+  'https://i.scdn.co/image/ab67616d0000b273f79b7f08426e2899a832d3e6',
+  'https://i.scdn.co/image/ab67616d0000b2734718e2b124f79258be7bc452',
+  'https://i.scdn.co/image/ab67616d0000b2733899712512f50a8d9e01e951',
+  'https://i.scdn.co/image/ab67616d0000b273daea95d4bb9f793d43ce5318',
+];
+
+List artistImages = [
+  'https://i.scdn.co/image/ab6761610000e5eba5205abffd84341e5bace828',
+  'https://i.scdn.co/image/ab67616d0000b273a91c10fe9472d9bd89802e5a',
+  'https://s2.glbimg.com/UR2M_ayBQ1QvmEdPJ9hPOIz5Zh4=/0x0:1400x1518/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/l/g/eDbSUBRLqGNlPPYi5J4A/adore2.jpg',
+  'https://midias.correiobraziliense.com.br/_midias/jpg/2021/12/28/157219203_291556112324230_5865424628317623372_n-7264468.jpg',
+  'https://web.portalsucesso.com.br/wp-content/uploads/2021/11/john-legend-1.jpg',
+  'https://akamai.sscdn.co/uploadfile/letras/fotos/d/0/c/e/d0cea01ba46441b0600d71d56e666dc3.jpg',
+];
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_spotify_clone/app/auth/splash_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    ),
+  );
+}
